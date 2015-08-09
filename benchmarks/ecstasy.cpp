8e72323fddc6e4f5c949b1d6b4b9e7c7a93fbd96
Lusito <core@roughael.net>
2015-08-09 17:50:02 +0200

    couple of changes

--- benchmarks/ecstasy.cpp
+++ benchmarks/ecstasy.cpp
@@ -4,13 +4,17 @@
 #include <random>
 
 namespace ecstasy_benchmarks {
-	struct ComponentMock1 : public Component<ComponentMock1> {
-		int values[256];
-	};
+	const int COMPONENT_VALUES = 8;
+	const int NUM_ENTITIES = 2 << 14;
 	
-	struct ComponentMock2 : public Component<ComponentMock2> {
-		float values[256];
-	};
+	struct ComponentMock1 : public Component<ComponentMock1> { int values[COMPONENT_VALUES]; };
+	struct ComponentMock2 : public Component<ComponentMock2> { float values[COMPONENT_VALUES]; };
+	struct ComponentMock3 : public Component<ComponentMock3> { int values[COMPONENT_VALUES]; };
+	struct ComponentMock4 : public Component<ComponentMock4> { float values[COMPONENT_VALUES]; };
+	struct ComponentMock5 : public Component<ComponentMock5> { int values[COMPONENT_VALUES]; };
+	struct ComponentMock6 : public Component<ComponentMock6> { float values[COMPONENT_VALUES]; };
+	struct ComponentMock7 : public Component<ComponentMock7> { int values[COMPONENT_VALUES]; };
+	struct ComponentMock8 : public Component<ComponentMock8> { float values[COMPONENT_VALUES]; };
 	
 	template<typename T, int I>
 	class EntitySystemMock : public IteratingSystem<EntitySystemMock<T, I>> {
@@ -20,7 +24,7 @@ namespace ecstasy_benchmarks {
 		
 		void processEntity(Entity *entity, float deltaTime) override {
 			auto *c = entity->get<T>();
-			for(int i=0; i<256; i++) {
+			for(int i=0; i<COMPONENT_VALUES; i++) {
 				c->values[i] += inc;
 			}
 		}
@@ -34,23 +38,31 @@ namespace ecstasy_benchmarks {
 			engine.addSystem<EntitySystemMock<ComponentMock1, -123>>();
 			engine.addSystem<EntitySystemMock<ComponentMock2, 123>>();
 			engine.addSystem<EntitySystemMock<ComponentMock2, -123>>();
+			engine.addSystem<EntitySystemMock<ComponentMock3, 123>>();
+			engine.addSystem<EntitySystemMock<ComponentMock3, -123>>();
+			engine.addSystem<EntitySystemMock<ComponentMock4, 123>>();
+			engine.addSystem<EntitySystemMock<ComponentMock4, -123>>();
+			engine.addSystem<EntitySystemMock<ComponentMock5, 123>>();
+			engine.addSystem<EntitySystemMock<ComponentMock5, -123>>();
+			engine.addSystem<EntitySystemMock<ComponentMock6, 123>>();
+			engine.addSystem<EntitySystemMock<ComponentMock6, -123>>();
+			engine.addSystem<EntitySystemMock<ComponentMock7, 123>>();
+			engine.addSystem<EntitySystemMock<ComponentMock7, -123>>();
+			engine.addSystem<EntitySystemMock<ComponentMock8, 123>>();
+			engine.addSystem<EntitySystemMock<ComponentMock8, -123>>();
 			
 			std::default_random_engine generator;
-			std::uniform_int_distribution<int> distribution(0,3);
-			for(int i=0; i<10000; i++) {
+			std::uniform_int_distribution<int> distribution(0,8);
+			for(int i=0; i<NUM_ENTITIES; i++) {
 				auto *e = engine.createEntity();
-				switch(distribution(generator)) {
-				case 1:
-					e->assign<ComponentMock1>();
-					break;
-				case 2:
-					e->assign<ComponentMock2>();
-					break;
-				case 3:
-					e->assign<ComponentMock1>();
-					e->assign<ComponentMock2>();
-					break;
-				}
+				if(distribution(generator) == 1) e->assign<ComponentMock1>();
+				if(distribution(generator) == 2) e->assign<ComponentMock2>();
+				if(distribution(generator) == 3) e->assign<ComponentMock3>();
+				if(distribution(generator) == 4) e->assign<ComponentMock4>();
+				if(distribution(generator) == 5) e->assign<ComponentMock5>();
+				if(distribution(generator) == 6) e->assign<ComponentMock6>();
+				if(distribution(generator) == 7) e->assign<ComponentMock7>();
+				if(distribution(generator) == 8) e->assign<ComponentMock8>();
 				engine.addEntity(e);
 			}
 		}
--- benchmarks/entityx.cpp
+++ benchmarks/entityx.cpp
@@ -4,14 +4,18 @@
 #include <random>
 
 namespace entityx_benchmarks {
-	using namespace entityx;
-	struct ComponentMock1 {
-		int values[256];
-	};
+	const int COMPONENT_VALUES = 8;
+	const int NUM_ENTITIES = 2 << 14;
 	
-	struct ComponentMock2 {
-		float values[256];
-	};
+	using namespace entityx;
+	struct ComponentMock1 { int values[COMPONENT_VALUES]; };
+	struct ComponentMock2 { float values[COMPONENT_VALUES]; };
+	struct ComponentMock3 { int values[COMPONENT_VALUES]; };
+	struct ComponentMock4 { float values[COMPONENT_VALUES]; };
+	struct ComponentMock5 { int values[COMPONENT_VALUES]; };
+	struct ComponentMock6 { float values[COMPONENT_VALUES]; };
+	struct ComponentMock7 { int values[COMPONENT_VALUES]; };
+	struct ComponentMock8 { float values[COMPONENT_VALUES]; };
 	
 	template<typename T, int I>
 	class EntitySystemMock : public System<EntitySystemMock<T, I>> {
@@ -21,7 +25,7 @@ namespace entityx_benchmarks {
 			ComponentHandle<T> c;
 			for (auto entity : entityManager.entities_with_components<T>(c))
 			{
-				for(int i=0; i<256; i++) {
+				for(int i=0; i<COMPONENT_VALUES; i++) {
 					c->values[i] += inc;
 				}
 			}
@@ -38,23 +42,32 @@ namespace entityx_benchmarks {
 			m_systems.add<EntitySystemMock<ComponentMock1, -123>>();
 			m_systems.add<EntitySystemMock<ComponentMock2, 123>>();
 			m_systems.add<EntitySystemMock<ComponentMock2, -123>>();
+			m_systems.add<EntitySystemMock<ComponentMock3, 123>>();
+			m_systems.add<EntitySystemMock<ComponentMock3, -123>>();
+			m_systems.add<EntitySystemMock<ComponentMock4, 123>>();
+			m_systems.add<EntitySystemMock<ComponentMock4, -123>>();
+			m_systems.add<EntitySystemMock<ComponentMock5, 123>>();
+			m_systems.add<EntitySystemMock<ComponentMock5, -123>>();
+			m_systems.add<EntitySystemMock<ComponentMock6, 123>>();
+			m_systems.add<EntitySystemMock<ComponentMock6, -123>>();
+			m_systems.add<EntitySystemMock<ComponentMock7, 123>>();
+			m_systems.add<EntitySystemMock<ComponentMock7, -123>>();
+			m_systems.add<EntitySystemMock<ComponentMock8, 123>>();
+			m_systems.add<EntitySystemMock<ComponentMock8, -123>>();
+			m_systems.configure();
 			
 			std::default_random_engine generator;
-			std::uniform_int_distribution<int> distribution(0,3);
-			for(int i=0; i<10000; i++) {
+			std::uniform_int_distribution<int> distribution(0,8);
+			for(int i=0; i<NUM_ENTITIES; i++) {
 				auto e = m_entities.create();
-				switch(distribution(generator)) {
-				case 1:
-					e.assign<ComponentMock1>();
-					break;
-				case 2:
-					e.assign<ComponentMock2>();
-					break;
-				case 3:
-					e.assign<ComponentMock1>();
-					e.assign<ComponentMock2>();
-					break;
-				}
+				if(distribution(generator) == 1) e.assign<ComponentMock1>();
+				if(distribution(generator) == 2) e.assign<ComponentMock2>();
+				if(distribution(generator) == 3) e.assign<ComponentMock3>();
+				if(distribution(generator) == 4) e.assign<ComponentMock4>();
+				if(distribution(generator) == 5) e.assign<ComponentMock5>();
+				if(distribution(generator) == 6) e.assign<ComponentMock6>();
+				if(distribution(generator) == 7) e.assign<ComponentMock7>();
+				if(distribution(generator) == 8) e.assign<ComponentMock8>();
 			}
 		}
 		void run(benchpress::context *ctx) {
